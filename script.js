58f648cea9f16e2f36d9578068f09f4de2f120a7
Lschaaf24 <Liam2.Schaaf@live.uwe.ac.uk>
2025-11-08 16:37:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+// Close modal when clicking outside
+window.onclick = function (event) {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
